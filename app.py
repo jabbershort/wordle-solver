a90dcff16beb6a291d5859d42aea6adf5b84c2cc
Matt Harrison <matt.harrison@atkinsglobal.com>
2022-01-10 16:12:52 +0000

    probability scorer

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 
 import os, random
+from string import ascii_lowercase
+from collections import Counter
+import collections
 
 def loadWords():
     filePath = os.path.dirname(os.path.realpath(__file__)) + "//words.txt"
@@ -43,7 +46,6 @@ def knownCharacters(words, known):
             continue
     return acceptableWords
 
-
 def possibleChars(words,knownPossibilites):
     flatPossibilities = [item for sublist in knownPossibilites for item in sublist]
     acceptableWords = []
@@ -55,21 +57,45 @@ def possibleChars(words,knownPossibilites):
         for j in flatPossibilities:
             if j not in word:
                 possibility = False
-
         if possibility:
             acceptableWords.append(word)
     return acceptableWords
 
-def startingWord():
+def characterProbability():
     words = loadWords()
-    ind = random.randint(0,len(words))
-    return words[ind]
+    countDict = {}
+    probDict = {}
+    letterSum = 0
+    for c in ascii_lowercase:
+        count = sum(map(lambda x : 1 if c in x else 0, words))
+        letterSum += count
+        countDict[c] = count
+    for c in countDict:
+        probDict[c] = countDict[c]/letterSum
+    return probDict
 
+def sortWordsByScore(words,probDict):
+    scoredDict = {}
+    for word in words:
+        score = 1
+        for c in word:
+            score *= probDict[c]
+        scoredDict[word] = score
+    sorted_dict = collections.OrderedDict(sorted(scoredDict.items(), key=lambda kv: kv[1],reverse=True))
+    sorted_words = []
+    for key in sorted_dict.keys():
+        sorted_words.append(key)
+    printNum = min(10,len(sorted_words))
+    print("There are {} solutions, with the top {} being {}".format(len(sorted_words),printNum,sorted_words[0:printNum]))
+    return sorted_words
 
 if __name__ == "__main__":
+    charProb = characterProbability()
+    print("First guess.")
+    sortWordsByScore(loadWords(), charProb)
     banList = ['s','t','m','a','n','d','y','f','c']
 
-    known = ['','o','r','','e']
+    known = ['','o','','','e']
 
     impossibleCharacters = [
         [],
@@ -80,8 +106,16 @@ if __name__ == "__main__":
         ]
 
     solutions1 = guessWord(banList)
+    print("After first filter, grey letters.") 
+    sortWordsByScore(solutions1, charProb)
     
-    solutions2 = knownCharacters(solutions1,known)  
+
+    solutions2 = knownCharacters(solutions1,known) 
+    print("After second filter, green letters") 
+    sortWordsByScore(solutions2, charProb)
+
 
     solutions3 = possibleChars(solutions2,impossibleCharacters)
-    print(solutions3)
+    print("After third filter, yellow letters")
+    sortWordsByScore(solutions3, charProb)
+
